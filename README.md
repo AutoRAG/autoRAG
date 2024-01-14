89817b2942eeb64716850d2b060df14b4a4341c9
smilingcalvin <smilingcalvin@gmail.com>
2024-01-14 15:42:13 -0800

    Add citation and update README

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # reauge
-Easy development for retrieval augmented generation system
+Reauge is a package for easy development for retrieval augmented generation system using command lines.
+
+## Install
+In the main repo folder, run
+```
+pip install -e .
+```
 
 ## Usage
 This repo uses [Hydra](https://hydra.cc/docs/intro/) to manage the configs. Please check conf/config.yaml for the default values and use Hydra arguments to modify the default values. 
 
+### setup OPENAI API key if you are using OPENAI API for LLM and embedding model
+```
+export OPENAI_API_KEY=<YOUR_OPENAI_KEY>
+```
+
+### Build an index from corpus
+Upload your documents where the answers can be retrieved from to `data/<app_name>/corpus` folder. You can name your app in `app_name`. Change the `app_name` field in conf/config.yaml to your app name. 
+```
+python -m reauge.indexer.build
+```
 ### Prepare a test dataset
 Given some annotated data (question, reference) pairs, you can use the following command to prepare test data.
 ```
 python -m reauge.data_builder.build_from_annotated_retrieval_data 
 ```
-### Build an index
+If you don't have annotated data, you can use the following command to generate some synthetic data from LLM. Note that this step may be costly if you use OPENAI API. Use a small `data_builder.generate_synthetic_query.from_num_nodes` value in `conf/config.yaml` to avoid high cost.
 ```
-python -m reauge.indexer.build
+python -m reauge.data_builder.generate_synthetic_query
 ```
+
 ### Evaluate the retriever
 ```
 python -m reauge.retriever.evaluate
@@ -22,3 +39,9 @@ Note that you need to be in the entry directory of this repo to run the chatbot.
 ```
 streamlit run reauge/synthesizer/render.py
 ```
+
+## TODO
+1. Add evaluation for synthesizer
+2. Optimize the code
+3. Add unit test
+4. Add advanced PDF preprocessor
--- reauge/synthesizer/render.py
+++ reauge/synthesizer/render.py
@@ -3,19 +3,31 @@ from llama_index import ServiceContext
 from llama_index import StorageContext, load_index_from_storage
 from omegaconf import DictConfig, OmegaConf
 import hydra
+from llama_index.query_engine import CitationQueryEngine
 
 @hydra.main(version_base=None, config_path="../../conf", config_name="config")
 def main(cfg: DictConfig):
-    index_dir = cfg.synthesizer.render.index_dir
-    app_description = cfg.synthesizer.render.app_description
-    
+    cur_cfg = cfg.synthesizer.render
+    index_dir = cur_cfg.index_dir
+    app_description = cur_cfg.app_description
+    citation_cfg = cur_cfg.citation_cfg
+
     service_context = ServiceContext.from_defaults()
     # rebuild storage context
     storage_context = StorageContext.from_defaults(persist_dir=index_dir)
     # load index
     index = load_index_from_storage(storage_context, service_context=service_context)
-
-    query_engine = index.as_query_engine(service_context=service_context, streaming=True)
+    if citation_cfg.enable_cite:
+        # service_context for the synthesizer is same as service_context of the index
+        query_engine = CitationQueryEngine.from_args(
+            index,
+            similarity_top_k=citation_cfg.similarity_top_k,
+            # here we can control how granular citation sources are, the default is 512
+            citation_chunk_size=citation_cfg.citation_chunk_size,
+            streaming=True,
+        )
+    else:
+        query_engine = index.as_query_engine(service_context=service_context, streaming=True)
 
     st.header("Chat with Your Documents (only support single-turn conversation now)")
 
@@ -40,6 +52,10 @@ def main(cfg: DictConfig):
             for ans in response.response_gen:
                 full_response += ans
                 message_placeholder.markdown(full_response + "▌")
+            if cur_cfg.enable_cite:
+                full_response += '\n\n### References\n\n'
+                for idx, ref in enumerate(response.source_nodes):
+                    full_response += f"[{idx+1}]\n\n" + "```\n\n" + ref.node.get_text() + "\n\n```\n\n"
             message_placeholder.markdown(full_response)
         message = {"role": "assistant", "content": full_response}
         st.session_state.messages.append(message) # Add response to message history
