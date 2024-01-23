b25b304dd836e96b73e97d919d72e41ac25f134a
Zhiben Chen <zhiben@takemobi.com>
2024-01-23 12:44:36 -0500

    reformat using black

--- autorag/synthesizer/render.py
+++ autorag/synthesizer/render.py
@@ -11,6 +11,7 @@ global_hydra = hydra.core.global_hydra.GlobalHydra()
 # Call the clear() method on the instance
 global_hydra.clear()
 
+
 # initialize the index
 @st.cache_data
 def init(index_dir, enable_cite, similarity_top_k, citation_chunk_size):
@@ -23,31 +24,42 @@ def init(index_dir, enable_cite, similarity_top_k, citation_chunk_size):
         # service_context for the synthesizer is same as service_context of the index
         query_engine = CitationQueryEngine.from_args(
             index,
-            similarity_top_k = similarity_top_k,
+            similarity_top_k=similarity_top_k,
             # here we can control how granular citation sources are, the default is 512
-            citation_chunk_size = citation_chunk_size,
+            citation_chunk_size=citation_chunk_size,
             streaming=True,
         )
     else:
-        query_engine = index.as_query_engine(service_context=service_context, streaming=True)
+        query_engine = index.as_query_engine(
+            service_context=service_context, streaming=True
+        )
     return query_engine
 
 
 def show_feedback_component(message_id):
     # if the component is already rendered on the webpage, do nothing
-    if message_id in [feedback["message_id"] for feedback in st.session_state.feedbacks]:
+    if message_id in [
+        feedback["message_id"] for feedback in st.session_state.feedbacks
+    ]:
         return
-    
+
     cols = st.columns([0.1, 1])
     with cols[0]:
         if st.button("👍", key=f"thumbs_up_{message_id}"):
             st.write("thanks!")
-            st.session_state.feedbacks.append({"message_id": message_id, "is_good": True, "feedback": ""})
+            st.session_state.feedbacks.append(
+                {"message_id": message_id, "is_good": True, "feedback": ""}
+            )
     with cols[1]:
         if st.button("👎", key=f"thumbs_down_{message_id}"):
-            reason = st.text_input("Please let us know why this response was not helpful", key=f"reason_{message_id}")
+            reason = st.text_input(
+                "Please let us know why this response was not helpful",
+                key=f"reason_{message_id}",
+            )
             if reason:
-                st.session_state.feedbacks.append({"message_id": message_id, "is_good": False, "feedback": reason})
+                st.session_state.feedbacks.append(
+                    {"message_id": message_id, "is_good": False, "feedback": reason}
+                )
                 st.write("thanks!")
 
 
@@ -57,22 +69,34 @@ def main(cfg: DictConfig):
     index_dir = cur_cfg.index_dir
     app_description = cur_cfg.app_description
     citation_cfg = cur_cfg.citation_cfg
-    query_engine = init(index_dir, citation_cfg.enable_cite, citation_cfg.similarity_top_k, citation_cfg.citation_chunk_size)
-    
+    query_engine = init(
+        index_dir,
+        citation_cfg.enable_cite,
+        citation_cfg.similarity_top_k,
+        citation_cfg.citation_chunk_size,
+    )
+
     st.header("Chat with Your Documents (only support single-turn conversation now)")
 
-    if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    if "messages" not in st.session_state.keys():  # Initialize the chat message history
         st.session_state.messages = [
-            {"role": "assistant", "content": f"Ask me a question about {app_description}!"}
+            {
+                "role": "assistant",
+                "content": f"Ask me a question about {app_description}!",
+            }
         ]
-    
+
     if "feedbacks" not in st.session_state.keys():
         st.session_state.feedbacks = []
 
-    if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    if prompt := st.chat_input(
+        "Your question"
+    ):  # Prompt for user input and save to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-    for message_id, message in enumerate(st.session_state.messages): # Display the prior chat messages
+    for message_id, message in enumerate(
+        st.session_state.messages
+    ):  # Display the prior chat messages
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
@@ -90,15 +114,21 @@ def main(cfg: DictConfig):
                 full_response += ans
                 message_placeholder.markdown(full_response + "▌")
             if citation_cfg.enable_cite:
-                full_response += '\n\n### References\n\n'
+                full_response += "\n\n### References\n\n"
                 for idx, ref in enumerate(response.source_nodes):
-                    full_response += f"[{idx+1}]\n\n" + "```\n\n" + ref.node.get_text() + "\n\n```\n\n"
+                    full_response += (
+                        f"[{idx+1}]\n\n"
+                        + "```\n\n"
+                        + ref.node.get_text()
+                        + "\n\n```\n\n"
+                    )
             message_placeholder.markdown(full_response)
         message = {"role": "assistant", "content": full_response}
-        st.session_state.messages.append(message) # Add response to message history
-        
+        st.session_state.messages.append(message)  # Add response to message history
+
         # Show feedback components to make sure it is displayed after the message is fully returned
         show_feedback_component(len(st.session_state.messages) - 1)
+
 
 if __name__ == "__main__":
     main()
