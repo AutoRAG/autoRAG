989779c72bb64d648d4af59a38c7b57e52a18fcb
HephaeTech <148020045+HephaeTech8@users.noreply.github.com>
2024-01-28 17:31:58 -0500

    Azure processor (#3)
    * Add paragraph processor and json loader.
    
    * Add table processor and ConfigSingleton
    
    * yaml setting change
    
    * Reformat code with Black
    
    * Add comments
    
    * remove ConfigSingleton
    
    * change path in yaml
    
    * Add docstring for sentence_splitter_args
    
    * Move DEFAULT_FILE_TYPE to config/conf
    
    * Reformat
    
    ---------
    
    Co-authored-by: Yi Zhang <zhangyieva@gmail.com>

--- autorag/indexer/build.py
+++ autorag/indexer/build.py
@@ -1,17 +1,35 @@
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 from llama_index import ServiceContext
 
+from .process.azure.output import AzureOutputProcessor
+
 from omegaconf import DictConfig, OmegaConf
 import hydra
 
 
 @hydra.main(version_base=None, config_path="../../conf", config_name="config")
 def main(cfg: DictConfig):
+
+    # Extracting specific configuration values from the loaded configuration.
     data_dir = cfg.indexer.build.data_dir
     index_dir = cfg.indexer.build.index_dir
-    service_context = ServiceContext.from_defaults()
-    documents = SimpleDirectoryReader(data_dir).load_data()
-    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+    data_processor = cfg.indexer.build.data_processor
+
+    # Processing documents based on the specified data_processor type.
+    if data_processor == "azure":
+        # Initialize a SentenceSplitter with the given arguments
+        sentence_splitter_args = cfg.indexer.build.node_parser.args.sentence_splitter
+        file_type = cfg.indexer.build.file_type
+        nodes = AzureOutputProcessor(data_dir, file_type, sentence_splitter_args).nodes
+        print(len(nodes))
+        index = VectorStoreIndex(nodes)
+    else:
+        documents = SimpleDirectoryReader(data_dir).load_data()
+        service_context = ServiceContext.from_defaults()
+        index = VectorStoreIndex.from_documents(
+            documents, service_context=service_context
+        )
+
     index.storage_context.persist(persist_dir=index_dir)
 
 
---
+++ autorag/indexer/process/azure/output.py
@@ -0,0 +1,62 @@
+"""
+Microsoft Azure AI Document Intelligence Output Processor
+Process all the azure-preanalyzed files from data directory.
+"""
+
+from ..utils.json import JsonFileLoader
+from .paragraph import AzureParagraphProcessor
+from .table import AzureTablesProcessor
+
+
+class AzureOutputProcessor:
+    """
+    Initializes the AzureOutputProcessor with a specified data directory.
+
+    This processor loads all files from the given directory using JsonFileLoader
+    and process them into TextNode objects.
+
+    :param data_dir: The directory containing JSON files to be processed.
+    :param file_type: The type of the processed files.
+    :param sentence_splitter_args: Arguments for the SentenceSplitter function.
+                                   This can include arguments like chunk_size,
+                                   chunk_overlap, or any other arguments that
+                                   SentenceSplitter expects.
+    """
+
+    def __init__(
+        self,
+        data_dir: str = None,
+        file_type: str = None,
+        sentence_splitter_args: dict = {},
+    ) -> None:
+        # Load all files from the specified directory
+        self.all_files = JsonFileLoader(data_dir).load()
+        self.file_type = file_type
+        # Process the loaded files into nodes
+        self.sentence_splitter_args = sentence_splitter_args
+        self.nodes = self.get_nodes()
+
+    def get_nodes(self) -> list:
+        nodes = []
+
+        # Process the loaded files
+        for file_name, file_content in self.all_files.items():
+            paragraphs_list = file_content.get("paragraphs", [])
+            tables_list = file_content.get("tables", [])
+            # Process paragraph data
+            if paragraphs_list:
+                paragraphs_nodes = AzureParagraphProcessor(
+                    paragraphs_list,
+                    file_name,
+                    self.file_type,
+                    self.sentence_splitter_args,
+                ).nodes
+                nodes += paragraphs_nodes
+            # Process table data
+            if tables_list:
+                table_content_nodes = AzureTablesProcessor(
+                    tables_list, file_name, self.file_type
+                ).nodes
+                nodes += table_content_nodes
+
+        return nodes
