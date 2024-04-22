9169990414eb54512b2d7e42dcf7108ae045acab
xin <frando0615@gmail.com>
2024-04-22 14:49:04 -0500

    update

--- text_to_sql/text2sql_demo.py
+++ text_to_sql/text2sql_demo.py
@@ -1,29 +1,17 @@
-import streamlit as st
+import os
 from llama_index.core import SQLDatabase
 from llama_index.llms.openai import OpenAI
-from sqlalchemy import (
-    create_engine,
-    MetaData
-)
+from sqlalchemy import create_engine, MetaData
 from llama_index.core.query_engine import NLSQLTableQueryEngine
 
 engine = create_engine("sqlite:///airbnb_ny.db")
 metadata_obj = MetaData()
 metadata_obj.reflect(engine)
 
-
+os.environ["OPENAI_API_KEY"] = "your_openai_api_key_here"
 llm = OpenAI(temperature=0.1, model="gpt-3.5-turbo")
 
 sql_database = SQLDatabase(engine, metadata=metadata_obj, include_tables=["airbnb_ny"])
-
-#print(response)
-# "Find accommodation near the train station."
-# st.set_page_config(page_title="text 2 sql", page_icon="🤖", layout="wide")
-
-# st.header("Turn text into sql query")
-
-# # Create a text input field
-# user_input = st.text_input("Enter some text")
 
 query_engine = NLSQLTableQueryEngine(
     sql_database=sql_database,
@@ -36,4 +24,3 @@ response = query_engine.query(query_str)
 print(response)
 print(response.metadata['sql_query'])
 print(response.metadata['result'])
-#st.write(f"The processed text is: {response}")
